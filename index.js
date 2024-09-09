3f9fab9e0f13a0ec3111c5db7e05a360a04966cd
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-09-09 23:08:59 +0600

    blog create status update and delete api are added

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  operationSuccessStatus: 200,
+};
+const app = express();
+app.use(express.json());
+app.use(cors(corsOptions));
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmhyihx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // collections
+    const blogsCollection = client.db("donorhive").collection("blogs");
+    // all apis
+
+    // Blog
+    app.get("/blogs", async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/blog", async (req, res) => {
+      const blogInfo = req.body;
+      const result = await blogsCollection.insertOne(blogInfo);
+      res.send(result);
+    });
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: status,
+      };
+      const result = await blogsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Donor hive server is running");
+});
+app.listen(port, () => {
+  console.log(`Donor hive server is running on port ${port}`);
+});
